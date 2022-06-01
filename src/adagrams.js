810a58d29824de66da22eccfaf83b514148bc2af
Liqingcathy <cathy.glc@gmail.com>
2022-06-01 14:35:32 -0700

    add two function implementation

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,13 +1,76 @@
+const LETTER_POOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const letterPoolCopy = Object.assign({}, LETTER_POOL);
+  //const letter_pool_copy = {...LETTER_POOL} ; //spread syntax
+  
+  const lettersDrawn = []
+  const letterKeys = Object.keys(letterPoolCopy);
+  while (lettersDrawn.length < 10){
+    let random_letter = Math.floor(Math.random()*lettersDrawn.length);
+    if (letterPoolCopy[random_letter] >= 1){
+      lettersDrawn.push(random_letter);
+      letterPoolCopy[random_letter] -= 1;
+    }
+  }
+  return lettersDrawn;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
+  const inputUpper = input.toUpperCase();
+  const countLetter = {};
+  for (let l of lettersInHand){
+    if (l in countLetter){
+      countLetter[l] += 1;
+    }else{
+      countLetter[l] = 1;
+    }
+  }
+
+  for (let char of inputUpper){
+    if (char in countLetter){
+      countLetter[char] -= 1;
+    }else{
+      return false;
+    }
+  }
+  return true;
+
 };
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+
 };
 
 export const highestScoreFrom = (words) => {
