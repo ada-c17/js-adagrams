9960f9b3072a66c30153a28fed31fb2dcfc5e2d0
Megan Korling <korlingm@gmail.com>
2022-06-01 15:58:04 -0700

    added drawLetters function and I think it passes the tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,45 @@
+const LETTER_POOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+const ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const hand = [];
+  const letterPoolCopy = { ...LETTER_POOL };
+  while (hand.length < 10) {
+    const num = Math.floor(Math.random() * 26);
+    const letter = ALPHABET[num];
+    if (letterPoolCopy[letter] > 0) {
+      hand.push(letter);
+      letterPoolCopy[letter]--;
+    }
+  }
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
