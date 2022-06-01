f3f77d27ad9b2eebf1b73d3dbf4f850b398394ec
Liqingcathy <cathy.glc@gmail.com>
2022-06-01 14:41:10 -0700

    add scoreWord implementation

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,35 @@ const LETTER_POOL = {
   Z: 1,
 };
 
+POINT_SYSTEM = {
+  'A':1,
+  'B':3,
+  'C':3,
+  'D':2,
+  'E':1,
+  'F':4,
+  'G':2,
+  'H':4,
+  'I':1,
+  'J':8,
+  'K':5,
+  'L':1,
+  'M':3,
+  'N':1,
+  'O':1,
+  'P':3,
+  'Q':10,
+  'R':1,
+  'S':1,
+  'T':1,
+  'U':1,
+  'V':4,
+  'W':4,
+  'X':8,
+  'Y':4,
+  'Z':10
+}
+
 
 export const drawLetters = () => {
   // Implement this method for wave 1
@@ -70,7 +99,14 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
-
+  const total = 0;
+  for (let w of word){
+    total += POINT_SYSTEM[w.toUpperCase()];
+  }
+  if (word.length > 6 && word.length < 11){
+    total += 8;
+  }
+  return total;
 };
 
 export const highestScoreFrom = (words) => {
