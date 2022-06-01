992b6590181ba45a83e3efcf702d504c4dabf619
sarah <skiyoko14@gmail.com>
2022-06-01 16:44:15 -0700

    implement scoreWord and test for score empty string

--- src/adagrams.js
+++ src/adagrams.js
@@ -113,6 +113,35 @@ const LETTERBANK = [
   "Z",
 ];
 
+const LETTERSCORES = {
+  A: 1,
+  E: 1,
+  I: 1,
+  O: 1,
+  U: 1,
+  L: 1,
+  N: 1,
+  R: 1,
+  S: 1,
+  T: 1,
+  D: 2,
+  G: 2,
+  B: 3,
+  C: 3,
+  M: 3,
+  P: 3,
+  F: 4,
+  H: 4,
+  V: 4,
+  W: 4,
+  Y: 4,
+  K: 5,
+  J: 8,
+  X: 8,
+  Q: 10,
+  Z: 10,
+};
+
 export const drawLetters = () => {
   // Implement this method for wave 1
   const randNums = [];
@@ -145,6 +174,18 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  let score = 0;
+  word = word.toUpperCase();
+  if (!word) {
+    return score;
+  }
+  for (let letter of word) {
+    score += LETTERSCORES[letter];
+  }
+  if (word.length > 6) {
+    score += 8;
+  }
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
