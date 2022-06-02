cc35738adb68983daa6330f9baadc60ae339db57
Shari Fedak <90287794+sharifedak12@users.noreply.github.com>
2022-06-02 16:57:40 -0400

    passes wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,15 +1,96 @@
+const SCORE_CHART = {
+    1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+    2: ["D", "G"],
+    3: ["B", "C", "M", "P"],
+    4: ["F", "H", "V", "W", "Y"],
+    5: ["K"],
+    8: ["J", "X"],
+    10: ["Q", "Z"],
+};
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+    const letterPool = {
+        A: 9,
+        B: 2,
+        C: 2,
+        D: 4,
+        E: 12,
+        F: 2,
+        G: 3,
+        H: 2,
+        I: 9,
+        J: 1,
+        K: 1,
+        L: 4,
+        M: 2,
+        N: 6,
+        O: 8,
+        P: 2,
+        Q: 1,
+        R: 6,
+        S: 4,
+        T: 6,
+        U: 4,
+        V: 2,
+        W: 2,
+        X: 1,
+        Y: 2,
+        Z: 1,
+    };
+    const letters = [
+        "A",
+        "B",
+        "C",
+        "D",
+        "E",
+        "F",
+        "G",
+        "H",
+        "I",
+        "J",
+        "K",
+        "L",
+        "M",
+        "N",
+        "O",
+        "P",
+        "Q",
+        "R",
+        "S",
+        "T",
+        "U",
+        "V",
+        "W",
+        "X",
+        "Y",
+        "Z",
+    ];
+    const randomLetters = (choices) =>
+        choices[Math.floor(Math.random() * choices.length)];
+    let hand = [];
+    for (let i = 0; i < 10; ++i) {
+        let letter = randomLetters(letters);
+        // Added a label here to break so it wouldn't go through the whole pool
+        // after adding the letter to the hand once it finds it in the pool
+        handPlacement: for (const [key, value] of Object.entries(letterPool)) {
+            if (letter == key && value > 0) {
+                letterPool[key] -= 1;
+                hand.push(letter);
+                break handPlacement;
+            }
+        }
+    }
+    return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+    // Implement this method for wave 2
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+    // Implement this method for wave 3
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 1
+    // Implement this method for wave 1
 };
