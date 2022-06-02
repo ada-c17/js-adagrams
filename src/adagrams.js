5c34d446f414ece0698167c33170191cd5ee32c7
Ruge Li <rugeli0605@gmail.com>
2022-06-01 22:45:49 -0700

    wave01 without testing

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,43 @@
+import { _ } from "core-js";
+
+const LETTER_POOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const lettersArray = [];
+  let dictIndex = 0;
+  while (lettersArray.filter(x => x === Object.keys(LETTER_POOL)[dictIndex]).length < Object.values(LETTER_POOL)[dictIndex]) {
+    lettersArray.push(Object.keys(LETTER_POOL)[dictIndex]);
+  };
+  return _.sample(lettersArray,10);
+
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
