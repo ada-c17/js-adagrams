6177a16262cf74d2ad3c6ab2b6afc05aab5fd6ef
Kiana <kianashui@gmail.com>
2022-06-01 20:59:18 -0700

    added drawLetters function to draw 10 random letters from the letter pool

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,50 @@
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const LETTER_POOL = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
+  };
+
+  const letterPool = [];
+  const drawnLetters = [];
+
+  for (const letter in LETTER_POOL) {
+    for (let i = 0; i < LETTER_POOL[letter]; i++) {
+      letterPool.push(letter);
+    }
+  }
+
+  for (let i = 0; i < 10; i++) {
+    const index = Math.floor(Math.random() * letterPool.length);
+    const letter = letterPool[index];
+    drawnLetters.push(letter);
+    letterPool.splice(index, 1);
+  }
+
+  return drawnLetters;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
