eb074e5b1b971f097412a503222c1f1f6f50751b
tinghj17 <tinghj17@gmail.com>
2022-06-01 21:46:20 -0700

    Draws 10 letters

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,20 @@
+const LETTER_POOL = [
+  "A", "A", "A", "A", "A", "A", "A", "A", "A", "B", "B", "C","C", "D", "D", "D",
+  "D", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "E", "F", "F","G", "G", "G", "H", "H", "I", "I", "I", "I", "I", "I", "I", "I", "I", "J", "K", "L", "L", "L", "L", "M", "M", "N", "N", "N", "N", "N", "N", "O", "O", "O", "O", "O", "O", "O", "O", "P", "P", "Q", "R", "R", "R", "R", "R", "R", "S", "S", "S", "S", "T", "T", "T", "T", "T", "T", "U", "U", "U", "U", "V", "V", "W", "W", "X", "Y", "Y", "Z"];
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const tenLetters = [];
+	const uniqueIndex = [];
+  while(tenLetters.length < 10) {
+		let index = Math.floor((Math.random()*LETTER_POOL.length));
+		if (!uniqueIndex.includes(index)) {
+			uniqueIndex.push(index);
+			const letter = LETTER_POOL[index];
+			tenLetters.push(letter);
+		}
+  }
+	return tenLetters;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
