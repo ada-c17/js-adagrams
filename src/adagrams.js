1ae41cee0f4012bca58775454d7667bef4eb61ca
Nikki Torab <nikkitorab@gmail.com>
2022-06-01 22:10:39 -0700

    wave 1 finished

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,48 @@
+const LETTERS = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const lettersCopy = { ...LETTERS };
+  const hand = [];
+  const letterArr = Object.keys(lettersCopy);
+  for (let i = 0; i < 10; i++) {
+    const letter = letterArr[Math.floor(Math.random() * letterArr.length)];
+
+    if (lettersCopy[letter] > 0) {
+      lettersCopy[letter] -= 1;
+      hand.push(letter);
+    } else {
+      i -= 1;
+    }
+  }
+  return hand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
