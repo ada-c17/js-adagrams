ec0e22f2dd19a664b272f83ca32a39c374b7939a
Amber <ambertanaka@yahoo.com>
2022-06-02 10:51:49 -0700

    Passing some tests honestly not sure

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,11 +1,53 @@
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const letterPool = {    
+  'A': 9, 
+  'B': 2, 
+  'C': 2, 
+  'D': 4, 
+  'E': 12, 
+  'F': 2, 
+  'G': 3, 
+  'H': 2, 
+  'I': 9, 
+  'J': 1, 
+  'K': 1, 
+  'L': 4, 
+  'M': 2, 
+  'N': 6, 
+  'O': 8, 
+  'P': 2, 
+  'Q': 1, 
+  'R': 6, 
+  'S': 4, 
+  'T': 6, 
+  'U': 4, 
+  'V': 2, 
+  'W': 2, 
+  'X': 1, 
+  'Y': 2, 
+  'Z': 1
 };
-
+  let copiedPool = JSON.parse(JSON.stringify(letterPool));
+  let chosenLetters = []
+    while (chosenLetters.length < 10) {
+      const keys = Object.keys(copiedPool);
+      let randomLetter = (keys[Math.floor(Math.random() * keys.length)]);
+      if (copiedPool[randomLetter] > 0) {
+        chosenLetters.push(randomLetter)
+        copiedPool[randomLetter] -= 1  
+  };
+}
+}
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
-};
-
+  for (let i in input) {
+    if (!(i in lettersInHand)) {
+      return false;
+  } else {
+      delete lettersInHand[i];
+  }
+  return true;
+}
+}
 export const scoreWord = (word) => {
   // Implement this method for wave 3
 };
