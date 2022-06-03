52ff82cb68ad69dfa60942a740e03d1c791407a8
Anna Pranovich <programming.aap@gmail.com>
2022-06-02 19:06:25 -1000

    finish wave1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,52 @@
+const letterPool = {
+  'A': 9, 
+  'B': 2, 
+  'C': 2, 
+  'D': 4, 
+  'E': 12, 
+  'F': 2, 
+  'G': 3, 
+  'H': 2, 
+  'I': 9, 
+  'J': 1, 
+  'K': 1, 
+  'L': 4, 
+  'M': 2, 
+  'N': 6, 
+  'O': 8, 
+  'P': 2, 
+  'Q': 1, 
+  'R': 6, 
+  'S': 4, 
+  'T': 6, 
+  'U': 4, 
+  'V': 2, 
+  'W': 2, 
+  'X': 1, 
+  'Y': 2, 
+  'Z': 1
+}
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const eachLetterPool = [];
+
+  for (const letter in letterPool) {
+		for (let i = 0; i < letterPool[letter]; i++) {
+			eachLetterPool.push(letter);
+    }
+  }
+
+  const handPool = []
+  while (handPool.length < 10) {
+    const randomLetter = eachLetterPool[Math.floor(Math.random() * eachLetterPool.length)];
+    handPool.push(randomLetter);
+    const indexToRemove = eachLetterPool.indexOf(randomLetter);
+		eachLetterPool.splice(indexToRemove, 1);
+		console.log(eachLetterPool.length)
+  }
+
+  return handPool;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
