4216285a1a1166465802e52a40c1f903f7c20966
Emily Schell <lauraemilyschell@gmail.com>
2022-06-03 11:18:25 -0400

    passes 41 tests including demo game

--- src/demo/adagrams.js
+++ src/demo/adagrams.js
@@ -1,56 +1,58 @@
-import {
-  drawLetters,
-  usesAvailableLetters,
-  scoreWord,
-  highestScoreFrom,
-} from "adagrams";
+import Adagrams from '../adagrams';
+import { letterPoints, letterQuants } from '../adagrams';
 
-const Real = {
-  drawLetters,
-  usesAvailableLetters,
-  scoreWord,
-  highestScoreFrom,
-};
+const Real = new Adagrams(letterQuants, letterPoints);
 
 const Stub = {
-  drawLetters() {
-    const defaultLetters = ["H", "E", "L", "L", "O", "W", "O", "R", "L", "D"];
+    drawLetters() {
+        const defaultLetters = [
+            'H',
+            'E',
+            'L',
+            'L',
+            'O',
+            'W',
+            'O',
+            'R',
+            'L',
+            'D',
+        ];
 
-    if (typeof Real.drawLetters === "function") {
-      return Real.drawLetters() || defaultLetters;
-    }
+        if (typeof Real.drawLetters === 'function') {
+            return Real.drawLetters() || defaultLetters;
+        }
 
-    return defaultLetters;
-  },
+        return defaultLetters;
+    },
 
-  usesAvailableLetters(input, lettersInHand) {
-    if (typeof Real.usesAvailableLetters === "function") {
-      return Real.usesAvailableLetters(input, lettersInHand);
-    }
+    usesAvailableLetters(input, lettersInHand) {
+        if (typeof Real.usesAvailableLetters === 'function') {
+            return Real.usesAvailableLetters(input, lettersInHand);
+        }
 
-    return true;
-  },
+        return true;
+    },
 
-  scoreWord(word) {
-    if (typeof Real.scoreWord === "function") {
-      return Real.scoreWord(word);
-    }
+    scoreWord(word) {
+        if (typeof Real.scoreWord === 'function') {
+            return Real.scoreWord(word);
+        }
 
-    return -1;
-  },
+        return -1;
+    },
 
-  highestScoreFrom(words) {
-    if (typeof Real.highestScoreFrom === "function") {
-      return Real.highestScoreFrom(words);
-    }
+    highestScoreFrom(words) {
+        if (typeof Real.highestScoreFrom === 'function') {
+            return Real.highestScoreFrom(words);
+        }
 
-    if (words.length < 1) return null;
+        if (words.length < 1) return null;
 
-    return {
-      word: words[0],
-      score: Stub.scoreWord(words[0]),
-    };
-  },
+        return {
+            word: words[0],
+            score: Stub.scoreWord(words[0]),
+        };
+    },
 };
 
 export default Stub;
