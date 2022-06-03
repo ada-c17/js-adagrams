f5f179badadceb3f3c7da63ea6e84cecc54bebbf
Natalya <chshebetun@gmail.com>
2022-06-03 11:37:07 -0700

    Completed Wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,44 @@
+const LETTERPOOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const lettersInHand = [];
+  const availableLetters = getAvailableLetters(LETTERPOOL);
+  let count = 0;
+
+  while (count < 10) {
+    let letter = getRandomLetter(availableLetters);
+    lettersInHand.push(letter);
+    count++;
+  }
+  return lettersInHand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
@@ -12,4 +51,24 @@ export const scoreWord = (word) => {
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 1
+};
+
+const getAvailableLetters = (letterData) => {
+  const characters = Object.keys(LETTERPOOL);
+  const generatedLetters = [];
+
+  characters.forEach((letter) => {
+    for (let i = 0; i < LETTERPOOL[letter]; i++) {
+      generatedLetters.push(letter);
+    }
+  });
+  return generatedLetters;
+};
+
+const getRandomLetter = (lettersToDraw) => {
+  let letterIndex = Math.floor(Math.random() * lettersToDraw.length);
+  let randomLetter = lettersToDraw[letterIndex];
+  lettersToDraw.splice(letterIndex, 1);
+
+  return randomLetter;
 };
