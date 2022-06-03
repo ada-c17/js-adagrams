a1317849df7a537209508a5a8e6efe2f0126652a
Anna Pranovich <programming.aap@gmail.com>
2022-06-02 19:57:51 -1000

    finish Wave2

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,34 +1,36 @@
-const letterPool = {
-  'A': 9, 
-  'B': 2, 
-  'C': 2, 
-  'D': 4, 
-  'E': 12, 
-  'F': 2, 
-  'G': 3, 
-  'H': 2, 
-  'I': 9, 
-  'J': 1, 
-  'K': 1, 
-  'L': 4, 
-  'M': 2, 
-  'N': 6, 
-  'O': 8, 
-  'P': 2, 
-  'Q': 1, 
-  'R': 6, 
-  'S': 4, 
-  'T': 6, 
-  'U': 4, 
-  'V': 2, 
-  'W': 2, 
-  'X': 1, 
-  'Y': 2, 
-  'Z': 1
-}
+import _ from "lodash";
 
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const letterPool = {
+    'A': 9, 
+    'B': 2, 
+    'C': 2, 
+    'D': 4, 
+    'E': 12, 
+    'F': 2, 
+    'G': 3, 
+    'H': 2, 
+    'I': 9, 
+    'J': 1, 
+    'K': 1, 
+    'L': 4, 
+    'M': 2, 
+    'N': 6, 
+    'O': 8, 
+    'P': 2, 
+    'Q': 1, 
+    'R': 6, 
+    'S': 4, 
+    'T': 6, 
+    'U': 4, 
+    'V': 2, 
+    'W': 2, 
+    'X': 1, 
+    'Y': 2, 
+    'Z': 1
+  }
+
   const eachLetterPool = [];
 
   for (const letter in letterPool) {
@@ -43,7 +45,6 @@ export const drawLetters = () => {
     handPool.push(randomLetter);
     const indexToRemove = eachLetterPool.indexOf(randomLetter);
 		eachLetterPool.splice(indexToRemove, 1);
-		console.log(eachLetterPool.length)
   }
 
   return handPool;
@@ -51,6 +52,28 @@ export const drawLetters = () => {
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
+
+  /**
+   * make a deep copy of lettersInHand to work with copy and modify it
+   * make empty checkingArray array to keep letter from word
+   * use loop. for each letter in word check if 'letters in hands' contains letter
+   *   if contains: 
+   *      push letter to chekingArray
+   *      remove letter from "letters in hand"
+   * check if length of checkingArray is equal the length of word:
+   *     return  true, otherwise return false
+   */
+  const handPool = _.cloneDeep(lettersInHand);
+  const checkingArray = [];
+
+  for (let letter of input) {
+    if (handPool.includes(letter)) {
+      checkingArray.push(letter);
+      handPool.splice(handPool.indexOf(letter), 1)
+    }
+  }
+
+  return checkingArray.length === input.length;
 };
 
 export const scoreWord = (word) => {
