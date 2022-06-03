46a1044f4ac369f1671453f331e1ae8f8cbe5aec
Morgan <morganheuer@gmail.com>
2022-06-03 13:24:50 -0700

    completed Wave 3 scoreWord

--- src/adagrams.js
+++ src/adagrams.js
@@ -99,6 +99,35 @@ const LETTER_POOL = [
   "Z",
 ];
 
+const letterValue = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
+
 export const drawLetters = () => {
   let letterPoolCopy = [...LETTER_POOL];
   let drawnLetters = [];
@@ -112,7 +141,6 @@ export const drawLetters = () => {
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
   for (let i = 0; i < input.length; i++) {
     if (!lettersInHand.includes(input[i])) {
       return false;
@@ -124,6 +152,15 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  let score = 0;
+  const upper = word.toUpperCase();
+  for (let i = 0; i < word.length; i++) {
+    score += letterValue[upper[i]];
+  }
+  if (word.length >= 7) {
+    score += 8;
+  }
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
