67a15d2fd6bd7266a9e4e56ca815cdf7c4c55847
dana-rocha <46910672+dana-rocha@users.noreply.github.com>
2022-06-03 17:33:07 -0700

    Creating letter distribution - Wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,41 +1,49 @@
-const LETTER_POOL = {
-  A: 9,
-  B: 2,
-  C: 2,
-  D: 4,
-  E: 12,
-  F: 2,
-  G: 3,
-  H: 2,
-  I: 9,
-  J: 1,
-  K: 1,
-  L: 4,
-  M: 2,
-  N: 6,
-  O: 8,
-  P: 2,
-  Q: 1,
-  R: 6,
-  S: 4,
-  T: 6,
-  U: 4,
-  V: 2,
-  W: 2,
-  X: 1,
-  Y: 2,
-  Z: 1,
-};
-
-
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const LETTER_POOL = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
+  };
 
   // Randomly draw 10 letters from the letter pool
   // Return: an array of ten string where each string is one letter
 
   // for loop from 1 to 10 
-  // Get key and value, save in another variable? decrement value 
+  // Get key and value, save in another variable
+
+  let letterDistribution = [];
+
+  for (const letter in LETTER_POOL) {
+    // console.log(letter);
+    for (let i = 0; i < LETTER_POOL[letter]; i++){
+      letterDistribution.push(letter);
+    }
+  }
+
+  console.log(letterDistribution);
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
