cfcaeccb1b7afce0aaadf4751a515919e726d392
Thu Vuong <tavuongg@gmail.com>
2022-06-04 13:29:59 -0700

    passed wave 1 and 2

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,13 +1,60 @@
+import {SCORE_CHART, LETTER_POOL} from './constants'
+
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  let letterPool = [];
+  for (const [letter, frequency] of Object.entries(LETTER_POOL)) {
+    for (let i = 0; i < frequency; i++) {
+      letterPool.push(letter);
+    }
+  }
+
+  let userHand = [];
+  for (let i = 0; i < 10; i++) {
+    const index = Math.floor(Math.random() * (letterPool.length - 1));
+    const letter = letterPool[index];
+    letterPool.splice(index, 1);
+    userHand.push(letter);
+  }
+
+  return userHand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  // create a new map with key being letter and value being frequency of what they used
+  if (input.length <= 0 || input.length > 10) {
+    return false;
+  }
+
+  let userLetter = {};
+  for (const char of input) {
+    if (char in userLetter) {
+      userLetter[char] += 1;
+    } else {
+      userLetter[char] = 1;
+    }
+  };
+  // check if the letter they used is  <= frequency
+  let userHand = {};
+  lettersInHand.forEach(letter => {
+    if (letter in userHand) {
+      userHand[letter] += 1;
+    } else {
+      userHand[letter] = 1;
+    }
+  });
+
+  for (const [letter, frequency] of Object.entries(userLetter)) {
+    if (!(letter in userHand && frequency <= userHand[letter])) {
+      return false;
+    }
+  }
+  
+  return true;
+
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  
 };
 
 export const highestScoreFrom = (words) => {
