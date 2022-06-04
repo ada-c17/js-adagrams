9f9b0816581f415a40a6c8052bf4b887cd328733
Morgan Adkisson <adkissonmorgan@gmail.com>
2022-06-04 14:11:01 -0700

    working implementation for drawLetters, passes all wave 01 tests... code is a bit clunky and could use some refactoring down the line.

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,45 @@
 export const drawLetters = () => {
-  // Implement this method for wave 1
+  const letterFreq = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
+  };
+  const drawn = [];
+  while (drawn.length < 10) {
+    let letters = Object.keys(letterFreq);
+    let randIndex = Math.floor(Math.random() * letters.length);
+    let randLetter = letters[randIndex];
+    console.log(randLetter);
+    let remainingLetterValue = letterFreq[randLetter];
+    if (remainingLetterValue > 0) {
+      drawn.push(randLetter);
+      letterFreq[randLetter] -= 1;
+    }
+  }
+  return drawn;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
