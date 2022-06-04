4701f5e16b39b40ae248515b3558898f9ebe8643
camilla <pasadenastringtheory@gmail.com>
2022-06-03 18:31:17 -0700

    Added comment

--- src/adagrams.js
+++ src/adagrams.js
@@ -31,11 +31,16 @@ export const drawLetters = () => {
   let userHand = []
   let availableLetters = []
 
-  // iterate through object with letter distribution to populate available_letters array
+  // iterate through object with letter distribution to populate availableLetters array
   for (let letter in letterDistribution) {
     availableLetters = availableLetters.concat(Array(letterDistribution[letter]).fill(letter));
   }
 
+  /*
+    build a userHand by looping 10 times, each time choosing a random index
+    in the 98 letters in the availableLetters array, and the letter at that index;
+    then remove chosen letter from availableLetters array, and add to userHand.
+  */
   for (var i = 0; i < 10; i++) {
     var chosenLetterIdx = Math.floor(Math.random() * availableLetters.length)
     // chosenLetter = availableLetters[chosenLetterIdx];
