ab8c47d166add14a94013c6a6f934f64b3914aa7
Juli <juli.terapin@gmail.com>
2022-06-04 17:13:49 -0400

    Completed Wave One

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,54 @@
+const letterQuantityData = {
+  A: 9,
+  N: 6,
+  B: 2,
+  O: 8,
+  C: 2,
+  P: 2,
+  D: 4,
+  Q: 1,
+  E: 12,
+  R: 6,
+  F: 2,
+  S: 4,
+  G: 3,
+  T: 6,
+  H: 2,
+  U: 4,
+  I: 9,
+  V: 2,
+  J: 1,
+  W: 2,
+  K: 1,
+  X: 1,
+  L: 4,
+  Y: 2,
+  M: 2,
+  Z: 1,
+};
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  // populate letterArr with the letters for fair probability
+  const letterArr = [];
+  for (const [letter, quantity] of Object.entries(letterQuantityData)) {
+    let counter = 0;
+    while (counter < quantity) {
+      letterArr.push(letter);
+      counter++;
+    }
+  }
+
+  // a loop that generates 10 random letters
+  // after generating a single letter, it is added to the letterBank
+  // and deleted off the letterArr for no repeated elements
+  const letterBank = [];
+  for (let i = 0; i < 10; i++) {
+    let letterIndex = Math.floor(Math.random() * letterArr.length);
+    letterBank.push(letterArr[letterIndex]);
+    letterArr.splice(letterIndex, 1);
+  }
+  return letterBank;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
@@ -11,5 +60,5 @@ export const scoreWord = (word) => {
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 1
+  // Implement this method for wave 4
 };
