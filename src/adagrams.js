3463329cabd9e7c52f8ed2022732f72a42367600
Jande Royalty <janderoyalty@gmail.com>
2022-06-04 00:19:30 -0500

    wave 04 complete

--- src/adagrams.js
+++ src/adagrams.js
@@ -120,4 +120,40 @@ export const scoreWord = (word) => {
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 1
+  let highestScore = 0;
+  let winningWord = "";
+
+  for (let word of words) {
+    let score = scoreWord(word);
+    if (score > highestScore) {
+      winningWord = word;
+      highestScore = score;
+    } else if (score === highestScore) {
+      if (winningWord.length === 10) continue;
+      else if (word.length === 10) {
+        winningWord = word;
+      } else if (word.length < winningWord.length) {
+        winningWord = word;
+      }
+    }
+  }
+  return { score: highestScore, word: winningWord };
+
+  // // PYTHON
+  // highest_score = 0
+  // winning_word = ""
+
+  // for word in word_list:
+  //     score = score_word(word)
+  //     if score > highest_score:
+  //         winning_word = word
+  //         highest_score = score
+  //     elif score == highest_score:
+  //         if len(winning_word) == 10:
+  //             continue
+  //         elif len(word) == 10:
+  //             winning_word = word
+  //         elif len(word) < len(winning_word):
+  //             winning_word = word
+  // return (winning_word, highest_score)
 };
