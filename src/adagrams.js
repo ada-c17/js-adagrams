e85aa58b73cf4dbfa985bddae11960c57c2546e5
Doina Colun <doinacolun@macpro.local>
2022-06-03 20:09:56 -0700

    Wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,18 @@
+import { LETTER_POOL } from "../test/adagrams.test";
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  let arr = [];
+
+  for (let key in LETTER_POOL) {
+      for (let i = 0; i < LETTER_POOL[key]; i++) {
+          arr.push(key);
+      }
+  }
+
+  arr.sort(() => Math.random() - 0.5);
+
+  return arr.slice(0, 10);
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
