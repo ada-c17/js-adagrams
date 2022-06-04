2ec65fb6ec197e02912a719b8fc6e46c1bbe5ec9
Myranae <myranae@pm.me>
2022-06-04 17:39:48 -0400

    completes first re-write to JS for w3

--- src/adagrams.js
+++ src/adagrams.js
@@ -84,6 +84,46 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  const scoreChart = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 2,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10,
+  };
+  const capWord = word.toUpperCase();
+  let totalScore = 0;
+  if (word === " ") {
+    return totalScore;
+  }
+  if (word.length >= 7 && word.length <= 10) {
+    totalScore += 8;
+  }
+  for (let letter of capWord) {
+    totalScore += scoreChart[letter];
+  }
+  return totalScore;
 };
 
 export const highestScoreFrom = (words) => {
