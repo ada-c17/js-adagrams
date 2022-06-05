0ce65e0ad1985a87ff9dac31627d292e176fd63f
Amber <ambertanaka@yahoo.com>
2022-06-05 15:28:25 -0700

    passing wave 2

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,53 +1,55 @@
 export const drawLetters = () => {
-  const letterPool = {    
-  'A': 9, 
-  'B': 2, 
-  'C': 2, 
-  'D': 4, 
-  'E': 12, 
-  'F': 2, 
-  'G': 3, 
-  'H': 2, 
-  'I': 9, 
-  'J': 1, 
-  'K': 1, 
-  'L': 4, 
-  'M': 2, 
-  'N': 6, 
-  'O': 8, 
-  'P': 2, 
-  'Q': 1, 
-  'R': 6, 
-  'S': 4, 
-  'T': 6, 
-  'U': 4, 
-  'V': 2, 
-  'W': 2, 
-  'X': 1, 
-  'Y': 2, 
-  'Z': 1
-};
-  let copiedPool = JSON.parse(JSON.stringify(letterPool));
-  let chosenLetters = []
-    while (chosenLetters.length < 10) {
-      const keys = Object.keys(copiedPool);
-      let randomLetter = (keys[Math.floor(Math.random() * keys.length)]);
-      if (copiedPool[randomLetter] > 0) {
-        chosenLetters.push(randomLetter)
-        copiedPool[randomLetter] -= 1  
+  const letterPool = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
   };
-}
-}
+  let copiedPool = JSON.parse(JSON.stringify(letterPool));
+  let chosenLetters = [];
+  while (chosenLetters.length < 10) {
+    const keys = Object.keys(copiedPool);
+    let randomLetter = keys[Math.floor(Math.random() * keys.length)];
+    if (copiedPool[randomLetter] > 0) {
+      chosenLetters.push(randomLetter);
+      copiedPool[randomLetter] -= 1;
+    }
+  }
+  return chosenLetters;
+};
 export const usesAvailableLetters = (input, lettersInHand) => {
   for (let i in input) {
-    if (!(i in lettersInHand)) {
-      return false;
-  } else {
+    if (lettersInHand.includes(input[i])) {
       delete lettersInHand[i];
+    } else {
+      return false;
+    }
   }
   return true;
-}
-}
+};
+
 export const scoreWord = (word) => {
   // Implement this method for wave 3
 };
