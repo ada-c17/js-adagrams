129d239e88e9603fd4a22d1add24ac52b5edf0bf
Sabrina Huwang <sabrinahuwang1@gmail.com>
2022-06-04 17:12:49 -0700

    Wave 1 finished

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,21 @@
+import Chance from "chance";
+
+const chance = new Chance();
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const letters = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split(
+    " "
+  );
+  const weights = [
+    9, 2, 2, 4, 12, 2, 3, 2, 9, 1, 1, 4, 2, 6, 8, 2, 1, 6, 4, 6, 4, 2, 2, 1, 2,
+    1,
+  ];
+
+  //https://stackoverflow.com/a/58525959
+  const bag = letters.flatMap((letter, i) => Array(weights[i]).fill(letter));
+
+  return chance.pickset(bag, 10);
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
