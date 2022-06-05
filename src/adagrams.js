228b0fb82efbe482b5e958a435e2bf8ca173f384
mit <mit.magh@gmail.com>
2022-06-04 21:11:57 -0700

    End of Wave 3 and completed test for an empty input

--- src/adagrams.js
+++ src/adagrams.js
@@ -61,7 +61,57 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  const SCORE_CHART = {
+    A: 1,
+    B: 3, 
+    C: 3, 
+    D: 2, 
+    E: 1, 
+    F: 4, 
+    G: 2, 
+    H: 4, 
+    I: 1,
+    J: 8, 
+    K: 5, 
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3, 
+    Q: 10,
+    R: 1, 
+    S: 1, 
+    T: 1, 
+    U: 1, 
+    V: 4,
+    W: 4, 
+    X: 8, 
+    Y: 4,
+    Z: 10
+  };
+
+  word = word.toUpperCase();
+  let score = 0;
+  for (const letter of word) {
+    score += Number(SCORE_CHART[letter]);
+  }
+  if (word.length > 6 && word.length <= 10) {
+    score += 8;
+  } else if (word.length == 0) {
+    return 0
+  }
+  return score
 };
+
+// word = word.upper()
+// score = 0
+// for letter in word:
+//     score += SCORE_CHART[letter]
+
+// if len(word) > 6:
+//     score += 8
+
+// return score
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 1
