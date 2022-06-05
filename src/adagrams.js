6092f6948031968fa2b9e0ba452f19754b9ffdba
olive <olivealavine@gmail.com>
2022-06-04 19:41:25 -0500

    wave 2 start

--- src/adagrams.js
+++ src/adagrams.js
@@ -26,6 +26,35 @@ const LETTER_POOL = {
   Y: 2,
   Z: 1,
 };
+const POINT_SCALE = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
+
 // Question: how to make this more efficient than nested loops?
 const makePoolOfLetters = () => {
   let poolOfLetters = [];
@@ -61,7 +90,32 @@ export const drawLetters = () => {
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  let availableLetters = {};
+  for (let letter of lettersInHand) {
+    if (letter in availableLetters) {
+      availableLetters[letter] += 1;
+    } else {
+      availableLetters[letter] = 1;
+    }
+  }
+  console.log(availableLetters);
+
+  for (let letter of input) {
+    if (letter in availableLetters) {
+      availableLetters[letter] -= 1;
+    } else {
+      return false;
+    }
+  }
+  console.log(`after : ${availableLetters}`);
+
+  for (let letter of availableLetters) {
+    console.log(letter);
+    if (availableLetters[letter] < 0) {
+      return false;
+    }
+  }
+  return true;
 };
 
 export const scoreWord = (word) => {
