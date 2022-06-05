7dd9a08fc90421a6ddd1af3e0e63c3799570724c
Nishat <nsalsabil25@gmail.com>
2022-06-05 10:02:05 -0700

    created a constants.js fill to import in LETTER_POOL, created drawLetters function and usesAvailableLetters functions, waves 1 and 2 passing

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,10 +1,50 @@
+
+import { LETTER_POOL } from "constants.js";
+
+
 export const drawLetters = () => {
   // Implement this method for wave 1
+  let letterPoolCopy = {...LETTER_POOL};
+  let hand = [];
+  while (hand.length < 10) {
+    let letter = Object.keys(letterPoolCopy)[Math.floor(Math.random()*Object.keys(letterPoolCopy).length)];
+    hand.push(letter);
+    letterPoolCopy[letter]--;
+    if (letterPoolCopy[letter] === 0) {
+      delete letterPoolCopy[letter];
+    }
+  }
+  return hand;
 };
+
+
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
+  const lettersInHandCopy = [...lettersInHand];
+  const inputArray = Array.from(input);
+
+  if (input.length > 10 || input.length < 1) {
+    return false;
+  }
+
+  for (const letter of inputArray) {
+    if (!lettersInHandCopy.includes(letter)) {
+      return false;
+    } else {
+      const index = lettersInHandCopy.indexOf(letter);
+      if (index > -1) {
+        lettersInHandCopy.splice(index, 1);
+      }
+    }
+  }
+  return true;
 };
+
+
+
+
+
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
