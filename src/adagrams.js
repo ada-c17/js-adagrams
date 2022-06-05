c741f961afc244781890d384edc7c27ca99ec301
Esther Annorzie <estherannorzie@gmail.com>
2022-06-05 15:07:57 -0400

    Created scoreChart variable to hold letter distribution

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,15 @@
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const scoreChart = {
+    1: ['J', 'K', 'Q', 'X', 'Z'],
+    2: ['B', 'C', 'F', 'H', 'M', 'P', 'V', 'W', 'Y'],
+    3: ['G'],
+    4: ['D', 'L', 'S', 'U'],
+    6: ['N', 'R', 'T'],
+    8: ['O'],
+    9: ['A', 'I'],
+    12: ['E']
+  }
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
