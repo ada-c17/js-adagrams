203c46fa4642050b1eef1ff9567c8ab9fb53bc01
Marlyn <marlynlopezz@gmail.com>
2022-06-05 22:13:46 -0700

    created scoreWord function to obtain scores for letters of word

--- src/adagrams.js
+++ src/adagrams.js
@@ -63,16 +63,57 @@ export const usesAvailableLetters = (input, lettersInHand) => {
       if (freqCounter[letter] === 0) {
         delete freqCounter[letter];
       }
-      // if (input[letter] === freqCounter) {
-      //   return true;
-      // }
     }
   }
+  for (const letter of input) {
+    if (typeof freqCounter[letter] !== "undefined") {
+      return false;
+    }
+  }
+  return true;
 };
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  const scoreDict = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10,
+  };
+  const make_upper = word.toUpperCase();
+  let score = 0;
+  for (const letter of make_upper) {
+    score += scoreDict[letter];
+  }
+  if (word.length >= 7 && word.length <= 11) {
+    score += 8;
+  }
+  return score;
 };
+
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 1
