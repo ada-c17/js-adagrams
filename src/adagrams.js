7613affda4bafe95a1ce5878f883ee3b8bcb8089
Megan Korling <korlingm@gmail.com>
2022-06-06 15:11:29 -0700

    added comments and changed variable names in highestScoreFrom

--- src/adagrams.js
+++ src/adagrams.js
@@ -60,8 +60,10 @@ export const drawLetters = () => {
   const hand = [];
   const letterPoolCopy = { ...LETTER_POOL };
   while (hand.length < 10) {
-    const num = Math.floor(Math.random() * 26);
-    const letter = ALPHABET[num];
+    // random number between 0 - 25 (inclusive)
+    const index = Math.floor(Math.random() * 26);
+    const letter = ALPHABET[index];
+    // add letter to user's hand only if available
     if (letterPoolCopy[letter] > 0) {
       hand.push(letter);
       letterPoolCopy[letter]--;
@@ -71,6 +73,9 @@ export const drawLetters = () => {
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
+  // check length of lettersInHand?
+
+  // object holiding count of available letters for user
   const lettersObj = {};
   for (let char of lettersInHand) {
     if (char in lettersObj) {
@@ -79,13 +84,17 @@ export const usesAvailableLetters = (input, lettersInHand) => {
       lettersObj[char] = 1;
     }
   }
+
+  // subtract from count of available letters based on input
   for (let char of input) {
     if (char in lettersObj) {
       lettersObj[char]--;
       if (lettersObj[char] < 0) {
+        // used too many of one letter
         return false;
       }
     } else {
+      // used letter not in hand
       return false;
     }
   }
@@ -99,33 +108,37 @@ export const scoreWord = (word) => {
   for (let char of inputWord) {
     points += LETTER_SCORES[char];
   }
-  if (word.length >= 7) {
-    // add 10 into this?
+  if (word.length >= 7 && word.length <= 10) {
     points += 8;
   }
   return points;
 };
 
 export const highestScoreFrom = (words) => {
-  // array of objects
+  // object for each word in list
+  // Could calculate the score in the for loop, but wanted to practice map
   const wordsAndScores = words.map((w) => ({ word: w, score: scoreWord(w) }));
+
   let highestScore = 0;
-  let highestPlay;
-  for (let play of wordsAndScores) {
-    if (play.score > highestScore) {
-      highestScore = play.score;
-      highestPlay = play;
-    } else if (play.score === highestScore && highestPlay.word.length < 10) {
+  let bestWord;
+  for (let currWord of wordsAndScores) {
+    // set new high score
+    if (currWord.score > highestScore) {
+      highestScore = currWord.score;
+      bestWord = currWord;
+
+      // tied high score
+    } else if (currWord.score === highestScore && bestWord.word.length < 10) {
       // order of precedence for ties
       // 1: first ten letter word
-      if (play.word.length === 10) {
-        highestPlay = play;
+      if (currWord.word.length === 10) {
+        bestWord = currWord;
       }
       // 2: first shortest word, if no word is 10 letters
-      else if (play.word.length < highestPlay.word.length) {
-        highestPlay = play;
+      else if (currWord.word.length < bestWord.word.length) {
+        bestWord = currWord;
       }
     }
   }
-  return highestPlay;
+  return bestWord;
 };
