e0a2fa2dcb196fc53681eb5ded570e329632a711
Taingleap Chheang <chheangtaingleap@gmail.com>
2022-06-06 15:09:55 -0700

    wave 5

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,118 +1,129 @@
-const letterPool = {
-  A: 9,
-  B: 2,
-  C: 2,
-  D: 4,
-  E: 12,
-  F: 2,
-  G: 3,
-  H: 2,
-  I: 9,
-  J: 1,
-  K: 1,
-  L: 4,
-  M: 2,
-  N: 6,
-  O: 8,
-  P: 2,
-  Q: 1,
-  R: 6,
-  S: 4,
-  T: 6,
-  U: 4,
-  V: 2,
-  W: 2,
-  X: 1,
-  Y: 2,
-  Z: 1,
-};
+class Adagrams {
+  constructor() {
+    this.letterPool = {
+      A: 9,
+      B: 2,
+      C: 2,
+      D: 4,
+      E: 12,
+      F: 2,
+      G: 3,
+      H: 2,
+      I: 9,
+      J: 1,
+      K: 1,
+      L: 4,
+      M: 2,
+      N: 6,
+      O: 8,
+      P: 2,
+      Q: 1,
+      R: 6,
+      S: 4,
+      T: 6,
+      U: 4,
+      V: 2,
+      W: 2,
+      X: 1,
+      Y: 2,
+      Z: 1,
+    };
 
-const scoreChart = {
-  A: 1,
-  B: 3,
-  C: 3,
-  D: 2,
-  E: 1,
-  F: 4,
-  G: 2,
-  H: 4,
-  I: 1,
-  J: 8,
-  K: 5,
-  L: 1,
-  M: 3,
-  N: 1,
-  O: 1,
-  P: 3,
-  Q: 10,
-  R: 1,
-  S: 1,
-  T: 1,
-  U: 1,
-  V: 4,
-  W: 4,
-  X: 8,
-  Y: 4,
-  Z: 10,
-};
-
-export const drawLetters = () => {
-  let letterList = [];
-
-  for (const key in letterPool) {
-    letterList = letterList.concat(Array(letterPool[key]).fill(key));
+    this.scoreChart = {
+      A: 1,
+      B: 3,
+      C: 3,
+      D: 2,
+      E: 1,
+      F: 4,
+      G: 2,
+      H: 4,
+      I: 1,
+      J: 8,
+      K: 5,
+      L: 1,
+      M: 3,
+      N: 1,
+      O: 1,
+      P: 3,
+      Q: 10,
+      R: 1,
+      S: 1,
+      T: 1,
+      U: 1,
+      V: 4,
+      W: 4,
+      X: 8,
+      Y: 4,
+      Z: 10,
+    };
+    this.handSize = 10;
+    this.bonusScore = 8;
   }
-  // shuffle the array
-  letterList.sort(() => Math.random() - 0.5);
 
-  const output = letterList.slice(0, 10);
+  drawLetters() {
+    let letterList = [];
 
-  return output;
-};
-
-export const usesAvailableLetters = (input, lettersInHand) => {
-  const inputUpper = input.toUpperCase();
-  const lettersInHandCopy = [...lettersInHand];
-
-  for (const letter of inputUpper) {
-    if (lettersInHandCopy.includes(letter)) {
-      lettersInHandCopy.splice(lettersInHandCopy.indexOf(letter), 1);
-    } else {
-      return false;
+    for (const key in this.letterPool) {
+      letterList = letterList.concat(Array(this.letterPool[key]).fill(key));
     }
+    // shuffle the array
+    letterList.sort(() => Math.random() - 0.5);
+
+    const output = letterList.slice(0, this.handSize);
+
+    return output;
   }
-  return true;
-};
 
-export const scoreWord = (word) => {
-  let wordArr = word.toUpperCase().split("");
-  let score = wordArr.reduce((total, char) => total + scoreChart[char], 0);
+  usesAvailableLetters(input, lettersInHand) {
+    const inputUpper = input.toUpperCase();
+    const lettersInHandCopy = [...lettersInHand];
 
-  if (word.length >= 7 && word.length <= 10) {
-    score += 8;
-  }
-  return score;
-};
-
-export const highestScoreFrom = (words) => {
-  let maxScore = 0;
-  let maxWord = null;
-
-  for (let i = 0; i < words.length; i++) {
-    const currentScore = scoreWord(words[i]);
-    if (currentScore > maxScore) {
-      maxScore = currentScore;
-      maxWord = words[i];
-    } else if (currentScore == maxScore && maxWord.length != 10) {
-      if (words[i].length < maxWord.length || words[i].length == 10) {
-        maxWord = words[i];
+    for (const letter of inputUpper) {
+      if (lettersInHandCopy.includes(letter)) {
+        lettersInHandCopy.splice(lettersInHandCopy.indexOf(letter), 1);
+      } else {
+        return false;
       }
     }
+    return true;
   }
-  const result = {
-    word: maxWord,
-    score: maxScore,
-  };
 
-  return result;
-};
+  scoreWord(word) {
+    let wordArr = word.toUpperCase().split("");
+    let score = wordArr.reduce(
+      (total, char) => total + this.scoreChart[char],
+      0
+    );
+
+    if (word.length >= 7 && word.length <= 10) {
+      score += this.bonusScore;
+    }
+    return score;
+  }
+
+  highestScoreFrom(words) {
+    let maxScore = 0;
+    let maxWord = null;
+
+    for (let i = 0; i < words.length; i++) {
+      const currentScore = this.scoreWord(words[i]);
+      if (currentScore > maxScore) {
+        maxScore = currentScore;
+        maxWord = words[i];
+      } else if (currentScore == maxScore && maxWord.length != 10) {
+        if (words[i].length < maxWord.length || words[i].length == 10) {
+          maxWord = words[i];
+        }
+      }
+    }
+    const result = {
+      word: maxWord,
+      score: maxScore,
+    };
+
+    return result;
+  }
+}
+
+export default Adagrams;
--- test/adagrams.test.js
+++ test/adagrams.test.js
@@ -1,9 +1,4 @@
-import {
-  drawLetters,
-  usesAvailableLetters,
-  scoreWord,
-  highestScoreFrom,
-} from "adagrams";
+import Adagrams from "adagrams";
 
 const LETTER_POOL = {
   A: 9,
@@ -35,166 +30,172 @@ const LETTER_POOL = {
 };
 
 describe("Adagrams", () => {
-  describe("drawLetters", () => {
-    it("draws ten letters from the letter pool", () => {
-      const drawn = drawLetters();
+  const game = new Adagrams();
 
-      expect(drawn).toHaveLength(10);
-    });
+  describe("Adagrams", () => {
+    describe("drawLetters", () => {
+      it("draws ten letters from the letter pool", () => {
+        const drawn = game.drawLetters();
 
-    it("returns an array, and each item is a single-letter string", () => {
-      const drawn = drawLetters();
-
-      expect(Array.isArray(drawn)).toBe(true);
-      drawn.forEach((l) => {
-        expect(l).toMatch(/^[A-Z]$/);
+        expect(drawn).toHaveLength(10);
       });
-    });
 
-    it("does not draw a letter too many times", () => {
-      for (let i = 0; i < 1000; i++) {
-        const drawn = drawLetters();
-        const letter_freq = {};
-        for (let letter of drawn) {
-          if (letter in letter_freq) {
-            letter_freq[letter] += 1;
-          } else {
-            letter_freq[letter] = 1;
+      it("returns an array, and each item is a single-letter string", () => {
+        const drawn = game.drawLetters();
+
+        expect(Array.isArray(drawn)).toBe(true);
+        drawn.forEach((l) => {
+          expect(l).toMatch(/^[A-Z]$/);
+        });
+      });
+
+      it("does not draw a letter too many times", () => {
+        for (let i = 0; i < 1000; i++) {
+          const drawn = game.drawLetters();
+          const letter_freq = {};
+          for (let letter of drawn) {
+            if (letter in letter_freq) {
+              letter_freq[letter] += 1;
+            } else {
+              letter_freq[letter] = 1;
+            }
+          }
+
+          for (let letter of drawn) {
+            expect(letter_freq[letter]).toBeLessThanOrEqual(
+              LETTER_POOL[letter]
+            );
           }
         }
-
-        for (let letter of drawn) {
-          expect(letter_freq[letter]).toBeLessThanOrEqual(LETTER_POOL[letter]);
-        }
-      }
-    });
-  });
-
-  describe("usesAvailableLetters", () => {
-    it("returns true if the submitted letters are valid against the drawn letters", () => {
-      const drawn = ["D", "O", "G", "X", "X", "X", "X", "X", "X", "X"];
-      const word = "DOG";
-
-      const isValid = usesAvailableLetters(word, drawn);
-      expect(isValid).toBe(true);
-    });
-
-    it("returns false when word contains letters not in the drawn letters", () => {
-      const drawn = ["D", "O", "X", "X", "X", "X", "X", "X", "X", "X"];
-      const word = "DOG";
-
-      const isValid = usesAvailableLetters(word, drawn);
-      expect(isValid).toBe(false);
-    });
-
-    it("returns false when word contains repeated letters more than in the drawn letters", () => {
-      const drawn = ["D", "O", "G", "X", "X", "X", "X", "X", "X", "X"];
-      const word = "GOOD";
-
-      const isValid = usesAvailableLetters(word, drawn);
-      expect(isValid).toBe(false);
-    });
-  });
-
-  describe("scoreWord", () => {
-    const expectScores = (wordScores) => {
-      Object.entries(wordScores).forEach(([word, score]) => {
-        expect(scoreWord(word)).toBe(score);
-      });
-    };
-
-    it("returns an accurate numerical score according to the score chart", () => {
-      expectScores({
-        A: 1,
-        DOG: 5,
-        WHIMSY: 17,
       });
     });
 
-    it("returns a score regardless of the input case", () => {
-      expectScores({
-        a: 1,
-        dog: 5,
-        wHiMsY: 17,
+    describe("usesAvailableLetters", () => {
+      it("returns true if the submitted letters are valid against the drawn letters", () => {
+        const drawn = ["D", "O", "G", "X", "X", "X", "X", "X", "X", "X"];
+        const word = "DOG";
+
+        const isValid = game.usesAvailableLetters(word, drawn);
+        expect(isValid).toBe(true);
+      });
+
+      it("returns false when word contains letters not in the drawn letters", () => {
+        const drawn = ["D", "O", "X", "X", "X", "X", "X", "X", "X", "X"];
+        const word = "DOG";
+
+        const isValid = game.usesAvailableLetters(word, drawn);
+        expect(isValid).toBe(false);
+      });
+
+      it("returns false when word contains repeated letters more than in the drawn letters", () => {
+        const drawn = ["D", "O", "G", "X", "X", "X", "X", "X", "X", "X"];
+        const word = "GOOD";
+
+        const isValid = game.usesAvailableLetters(word, drawn);
+        expect(isValid).toBe(false);
       });
     });
 
-    it("returns a score of 0 if given an empty input", () => {
-      expectScores({
-        "": 0,
-      });
-    });
-
-    it("adds an extra 8 points if word is 7 or more characters long", () => {
-      expectScores({
-        XXXXXXX: 64,
-        XXXXXXXX: 72,
-        XXXXXXXXX: 80,
-        XXXXXXXXXX: 88,
-      });
-    });
-  });
-
-  describe("highestScoreFrom", () => {
-    it("returns a hash that contains the word and score of best word in an array", () => {
-      const words = ["X", "XX", "XXX", "XXXX"];
-      const correct = { word: "XXXX", score: scoreWord("XXXX") };
-
-      expect(highestScoreFrom(words)).toEqual(correct);
-    });
-
-    it("accurately finds best scoring word even if not sorted", () => {
-      const words = ["XXX", "XXXX", "X", "XX"];
-      const correct = { word: "XXXX", score: scoreWord("XXXX") };
-
-      expect(highestScoreFrom(words)).toEqual(correct);
-    });
-
-    describe("in case of tied score", () => {
-      const expectTie = (words) => {
-        const scores = words.map((word) => scoreWord(word));
-        const highScore = scores.reduce((h, s) => (h < s ? s : h), 0);
-        const tiedWords = scores.filter((s) => s == highScore);
-
-        // Expect at least two tied words
-        expect(tiedWords.length).toBeGreaterThan(1);
+    describe("scoreWord", () => {
+      const expectScores = (wordScores) => {
+        Object.entries(wordScores).forEach(([word, score]) => {
+          expect(game.scoreWord(word)).toBe(score);
+        });
       };
 
-      it("selects the word with 10 letters", () => {
-        const words = ["AAAAAAAAAA", "BBBBBB"];
-        const correct = {
-          word: "AAAAAAAAAA",
-          score: scoreWord("AAAAAAAAAA"),
-        };
-        expectTie(words);
-
-        expect(highestScoreFrom(words)).toEqual(correct);
-        expect(highestScoreFrom(words.reverse())).toEqual(correct);
+      it("returns an accurate numerical score according to the score chart", () => {
+        expectScores({
+          A: 1,
+          DOG: 5,
+          WHIMSY: 17,
+        });
       });
 
-      it("selects the word with fewer letters when neither are 10 letters", () => {
-        const words = ["MMMM", "WWW"];
-        const correct = { word: "WWW", score: scoreWord("WWW") };
-        expectTie(words);
-
-        expect(highestScoreFrom(words)).toEqual(correct);
-        expect(highestScoreFrom(words.reverse())).toEqual(correct);
+      it("returns a score regardless of the input case", () => {
+        expectScores({
+          a: 1,
+          dog: 5,
+          wHiMsY: 17,
+        });
       });
 
-      it("selects the first word when both have same length", () => {
-        const words = ["AAAAAAAAAA", "EEEEEEEEEE"];
-        const first = {
-          word: "AAAAAAAAAA",
-          score: scoreWord("AAAAAAAAAA"),
-        };
-        const second = {
-          word: "EEEEEEEEEE",
-          score: scoreWord("EEEEEEEEEE"),
-        };
-        expectTie(words);
+      it("returns a score of 0 if given an empty input", () => {
+        expectScores({
+          "": 0,
+        });
+      });
 
-        expect(highestScoreFrom(words)).toEqual(first);
-        expect(highestScoreFrom(words.reverse())).toEqual(second);
+      it("adds an extra 8 points if word is 7 or more characters long", () => {
+        expectScores({
+          XXXXXXX: 64,
+          XXXXXXXX: 72,
+          XXXXXXXXX: 80,
+          XXXXXXXXXX: 88,
+        });
+      });
+    });
+
+    describe("highestScoreFrom", () => {
+      it("returns a hash that contains the word and score of best word in an array", () => {
+        const words = ["X", "XX", "XXX", "XXXX"];
+        const correct = { word: "XXXX", score: game.scoreWord("XXXX") };
+
+        expect(game.highestScoreFrom(words)).toEqual(correct);
+      });
+
+      it("accurately finds best scoring word even if not sorted", () => {
+        const words = ["XXX", "XXXX", "X", "XX"];
+        const correct = { word: "XXXX", score: game.scoreWord("XXXX") };
+
+        expect(game.highestScoreFrom(words)).toEqual(correct);
+      });
+
+      describe("in case of tied score", () => {
+        const expectTie = (words) => {
+          const scores = words.map((word) => game.scoreWord(word));
+          const highScore = scores.reduce((h, s) => (h < s ? s : h), 0);
+          const tiedWords = scores.filter((s) => s == highScore);
+
+          // Expect at least two tied words
+          expect(tiedWords.length).toBeGreaterThan(1);
+        };
+
+        it("selects the word with 10 letters", () => {
+          const words = ["AAAAAAAAAA", "BBBBBB"];
+          const correct = {
+            word: "AAAAAAAAAA",
+            score: game.scoreWord("AAAAAAAAAA"),
+          };
+          expectTie(words);
+
+          expect(game.highestScoreFrom(words)).toEqual(correct);
+          expect(game.highestScoreFrom(words.reverse())).toEqual(correct);
+        });
+
+        it("selects the word with fewer letters when neither are 10 letters", () => {
+          const words = ["MMMM", "WWW"];
+          const correct = { word: "WWW", score: game.scoreWord("WWW") };
+          expectTie(words);
+
+          expect(game.highestScoreFrom(words)).toEqual(correct);
+          expect(game.highestScoreFrom(words.reverse())).toEqual(correct);
+        });
+
+        it("selects the first word when both have same length", () => {
+          const words = ["AAAAAAAAAA", "EEEEEEEEEE"];
+          const first = {
+            word: "AAAAAAAAAA",
+            score: game.scoreWord("AAAAAAAAAA"),
+          };
+          const second = {
+            word: "EEEEEEEEEE",
+            score: game.scoreWord("EEEEEEEEEE"),
+          };
+          expectTie(words);
+
+          expect(game.highestScoreFrom(words)).toEqual(first);
+          expect(game.highestScoreFrom(words.reverse())).toEqual(second);
+        });
       });
     });
   });
