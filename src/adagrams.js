6224e2a99bc488bea52be010f24bd9379cc38633
Ruge Li <rugeli0605@gmail.com>
2022-06-06 15:49:09 -0700

    wave4 passed,comments added

--- src/adagrams.js
+++ src/adagrams.js
@@ -59,10 +59,9 @@ const LETTER_SCORE = {
 };
 
 export const drawLetters = () => {
-  // Implement this method for wave 1
   const lettersArray = [];
   let dictIndex = 0;
-
+// Use filter to compare if the times of the letter that has been pushed to lettersArray === its value   
   while (
     lettersArray.filter((x) => x === Object.keys(LETTER_POOL)[dictIndex])
       .length <= Object.values(LETTER_POOL)[dictIndex]
@@ -70,12 +69,12 @@ export const drawLetters = () => {
     lettersArray.push(Object.keys(LETTER_POOL)[dictIndex]);
     dictIndex++;
   }
-
+// draw 10 random letters from lettersArray using underscore.js 
   return _.sample(lettersArray, 10);
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
-  // Implement this method for wave 2
+  // make a copy of lettersInHand array for removing elements 
   const lettersInHandCopy = JSON.parse(JSON.stringify(lettersInHand));
   let uppercasePool = lettersInHandCopy.map((letter) => letter.toUpperCase());
   for (let i = 0; i < input.length; i++) {
@@ -89,7 +88,6 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
   const uppercaseWord = word.toUpperCase();
   let wordScore = 0;
   for (let letter of uppercaseWord){
@@ -102,13 +100,15 @@ export const scoreWord = (word) => {
 };
 
 export const highestScoreFrom = (words) => {
-  // Implement this method for wave 1
+  // create 2 variables to hold the best score so far and the best word so far 
   let bestScore = 0;
   let bestWord = null;
+  // iterate through words to find the highest scored word 
   for (let word of words){
     if (scoreWord(word) > bestScore){
       bestScore = scoreWord(word);
       bestWord = word;
+      // compare the length when there are ties 
     } else if (scoreWord(word) === bestScore && bestWord.length !== 10) {
       if (word.length === 10 || word.length < bestWord.length){
         bestWord = word;
@@ -117,3 +117,7 @@ export const highestScoreFrom = (words) => {
   }
   return {word:bestWord, score:bestScore};
 };
+
+// tried to encapsulate the function in a class and used static keyword, 
+// had to git stash wave5 because the first 4 waves got messed up so badly, and the deadline is approaching.
+// will need some more time to understand constructor, this, and the general concept as a whole. 
