5bb6b461efb1223dc59022d44478a0f3cfd953a3
Nikki Torab <nikkitorab@gmail.com>
2022-06-06 12:11:16 -0700

    wave 3 finished

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,45 @@ const LETTERS = {
   Z: 1,
 };
 
+// const SCORES = {
+//   1: ['A','E','O','U','L','N','R','S','T'],
+//   2: ['D','G'],
+//   3: ['B','C','M','P'],
+//   4: ['F','H','V','W','Y'],
+//   5: ['K'],
+//   8: ['J','X'],
+//   10: ['Q','Z'],
+// };
+
+const SCORES = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
+
 export const drawLetters = () => {
   // Implement this method for wave 1
   const lettersCopy = { ...LETTERS };
@@ -47,10 +86,32 @@ export const drawLetters = () => {
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
+  for (let i = 0; i < input.length; i++) {
+    if (lettersInHand.includes(input[i])) {
+      let letterIndex = lettersInHand.indexOf(input[i]);
+      lettersInHand.splice(letterIndex, 1);
+    } else {
+      return false;
+    }
+  }
+  return true;
 };
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  if (word.length < 1) {
+    return 0;
+  }
+  let score = 0;
+  if (word.length >= 7 && word.length <= 10) {
+    score += 8;
+  }
+  for (let i = 0; i < word.length; i++) {
+    let letterScore = SCORES[word[i].toUpperCase()];
+    score += letterScore;
+  }
+
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
