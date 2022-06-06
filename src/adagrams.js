0c16e5ed76c6310921197c72a87107dfe778d06c
mit <mit.magh@gmail.com>
2022-06-06 15:51:29 -0700

    Did some cleaning and checked Code coverage

--- src/adagrams.js
+++ src/adagrams.js
@@ -134,13 +134,3 @@ export const highestScoreFrom = (words) => {
   return bestWordAndScore
 
 };
-
-
-// let mobile = {
-//   brand: 'Samsung',
-//   model: 'Galaxy Note 9'
-// };
- 
-// for (let key in mobile) {
-//   console.log(`${key}: ${mobile[key]}`);
-// }
