af5d6d0fe716ed82546c9b02644dfe2a8661fa33
Esther Annorzie <estherannorzie@gmail.com>
2022-06-07 11:52:54 -0400

    Implemented scoreChart for  scoreWord

--- src/adagrams.js
+++ src/adagrams.js
@@ -85,6 +85,45 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  const scoreChart = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3, 
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10
+  };
+
+  let score = 0;
+  for (let i = 0; i < word.length, i++) {
+    if (i in scoreChart) {
+      score += scoreChart[i];
+    }
+  }
+  if (word.length >= 7 && word.length <=10) {
+    score += 8;
+  }
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
