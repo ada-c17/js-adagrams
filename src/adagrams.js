4efa01f23e7f39c3bfdc187319ec02156185a0d5
Nina <ninagetsbored@gmail.com>
2022-06-06 22:27:58 -0700

    passing 2/3 tests for wave 01

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,51 @@
-export const drawLetters = () => {
-  // Implement this method for wave 1
+const LETTER_POOL = {
+  'A': 9,
+  'B': 2,
+  'C': 2,
+  'D': 4,
+  'E': 12,
+  'F': 2,
+  'G': 3,
+  'H': 2,
+  'I': 9,
+  'J': 1,
+  'K': 1,
+  'L': 4,
+  'M': 2,
+  'N': 6,
+  'O': 8,
+  'P': 2,
+  'Q': 1,
+  'R': 6,
+  'S': 4,
+  'T': 6,
+  'U': 4,
+  'V': 2,
+  'W': 2,
+  'X': 1,
+  'Y': 2,
+  'Z': 1
+}
+
+const randomChoice = array => {
+  return array[Math.floor(array.length * Math.random())];
 };
+
+export const drawLetters = () => {
+  let hand = [];
+  let listOfLetters = Object.keys(LETTER_POOL);
+
+  while (hand.length < 10) {
+    let letter = randomChoice(listOfLetters);
+
+    // if (LETTER_POOL[letter] !== 0) {
+    //   LETTER_POOL[letter] --;
+    hand.push(letter);
+    // };
+  };
+  return hand;
+};
+
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
