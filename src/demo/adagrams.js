df743d795a2b0c4077a66d2414f5c344ab11d3fc
Sreymompech <sanchhen2013@gmail.com>
2022-06-06 20:13:41 -0700

    Import class Adagrams and create instance Real of Adagrams

--- src/demo/adagrams.js
+++ src/demo/adagrams.js
@@ -1,16 +1,18 @@
-import {
-  drawLetters,
-  usesAvailableLetters,
-  scoreWord,
-  highestScoreFrom,
-} from "adagrams";
+// import {
+//   drawLetters,
+//   usesAvailableLetters,
+//   scoreWord,
+//   highestScoreFrom,
+// } from "adagrams";
 
-const Real = {
-  drawLetters,
-  usesAvailableLetters,
-  scoreWord,
-  highestScoreFrom,
-};
+// const Real = {
+//   drawLetters,
+//   usesAvailableLetters,
+//   scoreWord,
+//   highestScoreFrom,
+// };
+import Adagrams from "../adagrams";
+const Real = Adagrams;
 
 const Stub = {
   drawLetters() {
