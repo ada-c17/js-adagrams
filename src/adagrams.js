c3c842e15c02b86a3e5d07f448a01f6126afce34
Joanna <joannaparis7@gmail.com>
2022-06-06 23:36:15 -0700

    wave 1

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,64 @@
+import { random } from "core-js/core/number";
+
+const LETTER_POOL = {
+  A: 9,
+  B: 2,
+  C: 2,
+  D: 4,
+  E: 12,
+  F: 2,
+  G: 3,
+  H: 2,
+  I: 9,
+  J: 1,
+  K: 1,
+  L: 4,
+  M: 2,
+  N: 6,
+  O: 8,
+  P: 2,
+  Q: 1,
+  R: 6,
+  S: 4,
+  T: 6,
+  U: 4,
+  V: 2,
+  W: 2,
+  X: 1,
+  Y: 2,
+  Z: 1,
+};
+
+const letterScores = {
+  1: ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T"],
+  2: ["D", "G"],
+  3: ["B", "C", "M", "P"],
+  4: ["F", "H", "V", "W", "Y"],
+  5: ["K"],
+  8: ["J", "X"],
+  10: ["Q", "Z"],
+};
+
+
+
 export const drawLetters = () => {
   // Implement this method for wave 1
-};
+  const letters = [];
+  const freq = {};
+  const lKeys = Object.keys(LETTER_POOL);
+  while (letters.length < 10) {
+    let randomL = lKeys[Math.floor(Math.random()*lKeys.length)];
+    if (randomL in freq) {
+      if (freq[randomL] < LETTER_POOL[randomL]){
+        freq[randomL]+=1;
+        letters.push(randomL);
+      }
+  } else {
+      freq[randomL] = 1;
+      letters.push[randomL];
+
+  }
+} return letters;
 
 export const usesAvailableLetters = (input, lettersInHand) => {
   // Implement this method for wave 2
