71bdfddb889c87c71474ee581e73f4a7cbb5534a
Joanna <joannaparis7@gmail.com>
2022-06-07 00:14:39 -0700

    wave 4 getting there

--- src/adagrams.js
+++ src/adagrams.js
@@ -73,10 +73,72 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   return true;
 };
 
+const chart = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
+
+
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  let score = 0;
+  if (word === undefined) {
+    return score;
+  }
+  word = word.toUpperCase();
+  for (const l of word) {
+    score += chart[l];
+  }
+  if (word.length > 6 && word.length < 11) {
+    score +=8;
+  }
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
   // Implement this method for wave 1
+  let win = {
+    word: "",
+    scoreW: 0,
+  };
+  for (const w of words) {
+    let wScore = scoreWord(word);
+    if (wScore > win.scoreW) {
+        win.word = word;
+        win.scoreW = wScore;
+    }
+    if (wScore=== win.scoreW && word.length>=10) {
+      win.word=word;
+      win.scoreW = wScore;
+      return win;
+  } else if (wScore === win.scoreW && word.length < win.word.length) {
+      win.word = word;
+      win.scoreW = wScore;
+  }
+}
+return win;
 };
