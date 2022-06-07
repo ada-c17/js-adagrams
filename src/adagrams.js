25f5c1c8a4c1a1406f21142b2e261c4dc68453f7
verasazonova <vera.zhokhova@gmail.com>
2022-06-06 21:50:50 -0700

    Done with wave 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -27,6 +27,35 @@ const letterPool = {
   Z: 1,
 };
 
+const scoreDict = {
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+};
+
 export const drawLetters = () => {
   // Implement this method for wave 1
   const letterPoolArray = [];
@@ -71,7 +100,6 @@ export const usesAvailableLetters = (input, lettersInHand) => {
   const letterBank = getLetterCount(lettersInHand);
 
   for (const [character, count] of Object.entries(wordCountDictionary)) {
-    console.log(`${character}: ${count}`);
     if (
       count > letterBank[character] ||
       !letterBank.hasOwnProperty(character)
@@ -84,6 +112,24 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  if (word.length === 0) {
+    return 0;
+  }
+
+  let score = 0;
+
+  for (let i = 0; i < word.length; i++) {
+    let upperLetter = word[i].toUpperCase();
+    if (scoreDict.hasOwnProperty(upperLetter)) {
+      score += scoreDict[upperLetter];
+    }
+  }
+  console.log(score);
+
+  if ([7, 8, 9, 10].includes(word.length)) {
+    score += 8;
+  }
+  return score;
 };
 
 export const highestScoreFrom = (words) => {
