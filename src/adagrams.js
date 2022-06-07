102e819440d18a657907401d2665c16e9c2fb9ef
Gabriela Webb <gabw813@gmail.com>
2022-06-07 09:18:20 -0700

    scoreWord function passes all tests

--- src/adagrams.js
+++ src/adagrams.js
@@ -126,6 +126,46 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  const scoreChart = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10,
+  };
+
+  const userInput = word.toUpperCase().split("");
+  let totalPoints = 0;
+  for (const letter of userInput) {
+    if (letter in scoreChart) {
+      totalPoints += scoreChart[letter];
+    }
+  }
+  if (userInput.length > 6 && userInput.length < 11) {
+    totalPoints += 8;
+  }
+  return totalPoints;
 };
 
 export const highestScoreFrom = (words) => {
