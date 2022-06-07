cf64583b781c0064504a0d5e629ba5493fea926e
Hillary Smith <hillaryhsmith@gmail.com>
2022-06-06 18:57:49 -0700

    implemented wave 3, test passed

--- src/adagrams.js
+++ src/adagrams.js
@@ -63,8 +63,10 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
   let lettersInHandCopy = [...lettersInHand];
 
-  for (let i = 0; i < input.length; i++) {
-    let index = lettersInHandCopy.indexOf(input[i]);
+  let upperInput = input.toUpperCase();
+
+  for (let i = 0; i < upperInput.length; i++) {
+    let index = lettersInHandCopy.indexOf(upperInput[i]);
     if (index !== -1) {
       lettersInHandCopy.splice(index, 1);
     } else {
@@ -76,6 +78,49 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+
+  const letterPoints = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1, 
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8, 
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1, 
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1, 
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10
+  };
+
+  let totalScore = 0;
+  
+  let upperWord = word.toUpperCase();
+  
+  for (let letter of upperWord) {
+    totalScore += letterPoints[letter];
+  };
+  
+  if (word.length >= 7) {
+    totalScore += 8;
+  };
+
+  return totalScore;
 };
 
 export const highestScoreFrom = (words) => {
