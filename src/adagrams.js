16b7178879593785e696326e46c1d7a9ba9f455a
Ada <adabarries@gmail.com>
2022-06-11 00:22:42 -0700

    drawLetters() completed, usesAvailableLetters() initialized

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,5 +1,46 @@
 export const drawLetters = () => {
   // Implement this method for wave 1
+  const LETTERPOOL = {
+    A: 9,
+    B: 2,
+    C: 2,
+    D: 4,
+    E: 12,
+    F: 2,
+    G: 3,
+    H: 2,
+    I: 9,
+    J: 1,
+    K: 1,
+    L: 4,
+    M: 2,
+    N: 6,
+    O: 8,
+    P: 2,
+    Q: 1,
+    R: 6,
+    S: 4,
+    T: 6,
+    U: 4,
+    V: 2,
+    W: 2,
+    X: 1,
+    Y: 2,
+    Z: 1,
+  };
+
+  let letterHand = [];
+  let letterObj = {...LETTERPOOL};
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  while (letterHand.length < 10) {
+    let randomNumber = Math.floor(Math.random() * 26);
+    let randomLetter = alphabet.charAt(randomNumber);
+    if (letterObj[randomLetter] > 0) {
+      letterHand.push(randomLetter);
+      letterObj[randomLetter] -= 1;
+    }
+  }
+  return letterHand;
 };
 
 export const usesAvailableLetters = (input, lettersInHand) => {
