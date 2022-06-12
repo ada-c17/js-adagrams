54f0edd45863df991fe2309c647dd09118249b00
Ada <adabarries@gmail.com>
2022-06-12 02:12:43 -0700

    implemented scoreWord(). All wave 3 tests pass.

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,4 +1,4 @@
-// import { set } from "core-js/core/dict";
+
 
 export const drawLetters = () => {
   // Implement this method for wave 1
@@ -60,11 +60,11 @@ export const usesAvailableLetters = (input, lettersInHand) => {
     return false;
   }
 
-  for (let letter in upperCaseInput) {
-    if (availableLetters.includes(upperCaseInput[letter]) === false) {
+  for (let i in upperCaseInput) {
+    if (availableLetters.includes(upperCaseInput[i]) === false) {
       return false
     } else {
-      let letterIndex = availableLetters.indexOf(upperCaseInput[letter]);
+      let letterIndex = availableLetters.indexOf(upperCaseInput[i]);
       availableLetters.splice(letterIndex, 1);
     }
   }
@@ -73,6 +73,49 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 
 export const scoreWord = (word) => {
   // Implement this method for wave 3
+  const scoreChart = {
+    A: 1,
+    B: 3,
+    C: 3,
+    D: 2,
+    E: 1,
+    F: 4,
+    G: 2,
+    H: 4,
+    I: 1,
+    J: 8,
+    K: 5,
+    L: 1,
+    M: 3,
+    N: 1,
+    O: 1,
+    P: 3,
+    Q: 10,
+    R: 1,
+    S: 1,
+    T: 1,
+    U: 1,
+    V: 4,
+    W: 4,
+    X: 8,
+    Y: 4,
+    Z: 10,
+  };
+  let wordScore = 0;
+
+  if (word === ''){
+    return 0;
+  }else {
+    const upperCaseWord = word.toUpperCase();
+    if (word.length >= 7) {
+      wordScore += 8;
+    }
+    for (let i in upperCaseWord) {
+      let currentLetter = upperCaseWord[i];
+      wordScore += scoreChart[currentLetter];
+    }
+    return wordScore;
+  }
 };
 
 export const highestScoreFrom = (words) => {
